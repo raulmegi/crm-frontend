8c7267329fcf2a01208906d5ab8e1030a390755e
Tonilis <tonilis@msn.com>
2025-06-28 13:11:14 +0200

    FRONT. Añadida busqueda a Contactos

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -6,6 +6,8 @@ import { NgIf, NgForOf } from '@angular/common';
 import { ContactPopupComponent } from '../contact-popup/contact-popup.component';
 import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
 
 @NgModule({
   imports: [
@@ -28,26 +30,49 @@ export class ContactComponent implements OnInit {
   error= '';
   selectedContact: Contact | null = null;
   modoPopup: 'NEW' | 'EDIT' | 'CLOSED' = 'CLOSED';
+  searchControl = new FormControl('');
+  filteredContacts: Contact[] = [];
 
   constructor(
     private contactService: ContactService) {}
 
     async ngOnInit(): Promise<void> {
-        await this.loadContacts();
-        }
+      await this.loadContacts();
 
+      // Búsqueda reactiva
+      this.searchControl.valueChanges
+        .pipe(
+          debounceTime(300),
+          distinctUntilChanged(),
+          switchMap(term => this.contactService.findByName(term || ''))
+        )
+        .subscribe({
+          next: response => {
+            if (isOkResponse(response)) {
+              this.filteredContacts = loadResponseData(response);
+            } else {
+              this.filteredContacts = [];
+              this.error = loadResponseError(response);
+            }
+          },
+          error: () => {
+            this.filteredContacts = [];
+            this.error = 'Error al buscar contactos';
+          }
+        });
+      }
 
-
-  async loadContacts(): Promise<void> {
-    this.error = '';
-    const response = await this.contactService.getContacts();
-    if (isOkResponse(response)) {
-      this.contacts = loadResponseData(response);
+    async loadContacts(): Promise<void> {
+      this.error = '';
+      const response = await this.contactService.getContacts();
+      if (isOkResponse(response)) {
+        this.contacts = loadResponseData(response);
+        this.filteredContacts = this.contacts; // Mostrar todos al inicio
+      } else {
+        this.error = loadResponseError(response);
+        this.filteredContacts = [];
+      }
     }
-  else {
-      this.error = loadResponseError(response);
-    }
-   }
 
  newContact(){
    this.selectedContact = null;
