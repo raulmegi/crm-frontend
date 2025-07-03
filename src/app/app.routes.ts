1580cf6ce6d271ca2d99dda0b2ecaaf5938fef1c
Narrmora <29nroman11@gmail.com>
2025-07-03 05:56:43 +0200

    autGuard implementation - needs testing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,5 @@
 // src/app/app.routes.ts
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
 import { ContactComponent } from './components/contact/contact.component';
@@ -9,26 +9,54 @@ import { CustomerListComponent } from './components/customer-list/customer-list.
 import { HomePageComponent } from './components/layout/home-page/home-page.component';
 import ConstRoutes from './shared/constants/const-routes';
 import { AppUserManagerComponent } from './components/app-user-manager/app-user-manager.component';
-
+import { AuthGuard } from './auth.guard';
+import { NgModule } from '@angular/core';
 
 export const appRoutes: Routes = [
   // Ruta raíz redirige al login
   { path: '', redirectTo: ConstRoutes.PATH_LOGIN, pathMatch: 'full' },
 
   // Rutas públicas
-  { path: ConstRoutes.PATH_LOGIN,  component: LoginComponent },
+  { path: ConstRoutes.PATH_LOGIN, component: LoginComponent },
   { path: ConstRoutes.PATH_SIGNUP, component: SignupComponent },
 
   // Home (privada, tras login)
   { path: 'home', component: HomePageComponent },
 
   // Rutas protegidas
-  { path: ConstRoutes.PATH_TASKS,    component: TaskListComponent },
-  { path: ConstRoutes.PATH_BRAND,    component: BrandListComponent },
+  { path: ConstRoutes.PATH_TASKS, component: TaskListComponent },
+  { path: ConstRoutes.PATH_BRAND, component: BrandListComponent },
   { path: ConstRoutes.PATH_CUSTOMER, component: CustomerListComponent },
-  { path: ConstRoutes.PATH_APPUSER,     component: AppUserManagerComponent },
+  { path: ConstRoutes.PATH_APPUSER, component: AppUserManagerComponent },
   { path: ConstRoutes.PATH_CONTACT, component: ContactComponent },
   { path: ConstRoutes.PATH_CONTACT, component: ContactComponent },
   // Comodín para cualquier ruta no encontrada
-  { path: '**', redirectTo: 'home' }
+  { path: '**', redirectTo: 'home' },
+  {
+    path: 'home',
+    component: HomePageComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: ConstRoutes.PATH_APPUSER,
+    component: AppUserManagerComponent,
+    canActivate: [AuthGuard]
+  },
+  {
+    path: ConstRoutes.PATH_LOGIN,
+    component: LoginComponent
+  },
+  {
+    path: '**',
+    redirectTo: 'home'
+  }
 ];
+
+@NgModule({
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
+
+
