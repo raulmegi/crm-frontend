6edbee6f9e826bd6d61357f393189ab1c4d822e9
Narrmora <29nroman11@gmail.com>
2025-07-03 07:38:30 +0200

    snackbar style

--- src/services/snackbar.service.ts
+++ src/services/snackbar.service.ts
@@ -1,16 +1,18 @@
-// src/app/services/snackbar.service.ts
 import { Injectable } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
+
 
 @Injectable({ providedIn: 'root' })
 export class SnackbarService {
   constructor(private snackBar: MatSnackBar) {}
 
-  show(message: string, action: string = 'Cerrar', duration: number = 3000) {
-    this.snackBar.open(message, action, {
+
+  show(message: string, duration = 3000) {
+    this.snackBar.open(message, '✕', {
       duration,
-      horizontalPosition: 'right',
-      verticalPosition: 'top'
+      horizontalPosition: 'center',
+      verticalPosition: 'top',
+      panelClass: ['custom-snackbar']
     });
   }
 }
